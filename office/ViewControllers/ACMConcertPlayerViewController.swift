85a707a3b557423889f5dc5e797a73402cac0f34
Sujay <sujay.patwardhan@gmail.com>
2018-04-13 20:18:07 -0500

    progress bar broken, added delegate

--- office/ViewControllers/ACMConcertPlayerViewController.swift
+++ office/ViewControllers/ACMConcertPlayerViewController.swift
@@ -30,16 +30,14 @@ final class ACMConcertPlayerViewController: UIViewController {
     @IBOutlet weak var skipButton: UIButton!
     @IBOutlet weak var viewQueueButton: UIButton!
 
-    weak var delegate: ACMConcertSocketDelegate?
-    let socketManager = SocketManager(socketURL: URL(string: "http://concert.acm.illinois.edu")!)
-    let jsonDecoder = JSONDecoder()
+    lazy var acmConcertSocket = ACMConcertSocket(delegate: self)
+    
     var timer: Timer?
     var duration = 1
     var progress = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureSocket()
 
         waveView.realWaveColor = UIColor.white.withAlphaComponent(1)
         waveView.maskWaveColor = UIColor.white.withAlphaComponent(0.3)
@@ -73,141 +71,38 @@ final class ACMConcertPlayerViewController: UIViewController {
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        setupSocket()
+        acmConcertSocket.setupSocket()
         NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(setupSocket),
+            acmConcertSocket,
+            selector: #selector(ACMConcertSocket.setupSocket),
             name: .UIApplicationDidBecomeActive, object: nil
         )
         NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(teardownSocket),
+            acmConcertSocket,
+            selector: #selector(ACMConcertSocket.teardownSocket),
             name: .UIApplicationWillResignActive, object: nil
         )
     }
 
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        NotificationCenter.default.removeObserver(self, name: .UIApplicationDidBecomeActive, object: nil)
-        NotificationCenter.default.removeObserver(self, name: .UIApplicationWillResignActive, object: nil)
-        teardownSocket()
-    }
-
-    // MARK: Socket Init
-    func configureSocket() {
-        socketManager.defaultSocket.on("connected",      callback: handleConnection)
-        socketManager.defaultSocket.on("heartbeat",      callback: handleConnection)
-        socketManager.defaultSocket.on("skipped",        callback: handleConnection)
-        socketManager.defaultSocket.on("volume_changed", callback: handleVolume)
-        socketManager.defaultSocket.on("paused",         callback: handlePause)
-        socketManager.defaultSocket.on("played",         callback: handlePlay)
-    }
-
-    @objc func setupSocket() {
-        socketManager.connect()
-    }
-
-    @objc func teardownSocket() {
-        socketManager.disconnect()
+        NotificationCenter.default.removeObserver(acmConcertSocket, name: .UIApplicationDidBecomeActive, object: nil)
+        NotificationCenter.default.removeObserver(acmConcertSocket, name: .UIApplicationWillResignActive, object: nil)
+        acmConcertSocket.teardownSocket()
     }
 
     // MARK: Actions
-    @IBAction func button() {
-        socketManager.defaultSocket.emit("pause")
-    }
-
-    @IBAction func didChangeVolume() {
-        let volume = Int(volumeSlider.value)
-        socketManager.defaultSocket.emit("volume", volume)
-    }
-
-    // MARK: Handlers
-    func handleConnection(dataArray: [Any], ack: SocketAckEmitter) {
-        print("handle connection a")
-
-        guard let jsonString = dataArray.first as? String,
-            let jsonData = jsonString.data(using: .utf8),
-            let status = try? jsonDecoder.decode(ACMConcertOnConnect.self, from: jsonData) else { return }
-
-        print("handle connection b")
-        
-        duration = status.duration
-        progress = status.currentTime
-        if timer?.isValid == true {
-            timer = Timer.scheduledTimer(
-                timeInterval: 1,
-                target: self,
-                selector: #selector(updateProgress),
-                userInfo: nil,
-                repeats: true
-            )
-        }
-
-        DispatchQueue.main.async { [weak self] in
-            self?.updateVolume(with: status.volume)
-            self?.updateArtwork(with: status.thumbnail)
-            self?.updatePlayPause(with: status.isPlaying)
-            self?.updateInfoLabel(with: status.currentTrack)
-        }
-    }
+//    @IBAction func button() {
+//        socketManager.defaultSocket.emit("pause")
+//    }
+//
+//    @IBAction func didChangeVolume() {
+//        let volume = Int(volumeSlider.value)
+//        socketManager.defaultSocket.emit("volume", volume)
+//    }
     
-    func handleVolume(dataArray: [Any], ack: SocketAckEmitter) {
-        print("handle volume a")
-
-        guard let jsonString = dataArray.first as? String,
-            let jsonData = jsonString.data(using: .utf8),
-            let status = try? jsonDecoder.decode(ACMConcertVolume.self, from: jsonData) else { return }
-
-        print("handle volume b")
-
-        let volume = status.volume
-        
-        DispatchQueue.main.async { [weak self] in
-            self?.updateVolume(with: volume)
-        }
-    }
-    
-    func handlePause(dataArray: [Any], ack: SocketAckEmitter) {
-        print("handle pause a")
-
-        guard let jsonString = dataArray.first as? String,
-            let jsonData = jsonString.data(using: .utf8),
-            let status = try? jsonDecoder.decode(ACMConcertOnPause.self, from: jsonData) else { return }
-
-        print("handle pause b")
-
-        let isPlaying = status.isPlaying,
-        audioStatus = status.audioStatus
-        let displayIsPlaying = isPlaying && (audioStatus == "State.Playing" || audioStatus == "State.Opening")
-        print(displayIsPlaying)
-        
-        DispatchQueue.main.async { [weak self] in
-            self?.updatePlayPause(with: displayIsPlaying)
-        }
-    }
-    
-    func handlePlay(dataArray: [Any], ack: SocketAckEmitter) {
-        print("handle play a")
-
-        guard let jsonString = dataArray.first as? String,
-            let jsonData = jsonString.data(using: .utf8),
-            let status = try? jsonDecoder.decode(ACMConcertOnPlay.self, from: jsonData) else { return }
-
-        print("handle play b")
-
-        let isPlaying = status.isPlaying,
-        audioStatus = status.audioStatus
-        let displayIsPlaying = isPlaying && (audioStatus == "State.Playing" || audioStatus == "State.Opening")
-        print(displayIsPlaying)
-        
-        DispatchQueue.main.async { [weak self] in
-            self?.updatePlayPause(with: displayIsPlaying)
-        }
-    }
-    
-    func updateArtwork(with artworkUrl: String) {
+    func updateArtwork(with url: URL?) {
         DispatchQueue.global(qos: .userInitiated).async {
-            let url = URL(string: "http://concert.acm.illinois.edu/" + artworkUrl)
             URLSession.shared.dataTask(with: url!) { data, response, error in
                 guard let data = data else { return }
 
@@ -216,7 +111,8 @@ final class ACMConcertPlayerViewController: UIViewController {
                 let colors = NSArray(ofColorsFrom: image, withFlatScheme: false) as! [UIColor]
 
                 DispatchQueue.main.async { [weak self] in
-                    self?.update(artworkImage: image, withBlurredArtworkImage: blurredImage)
+                    self?.artworkImageView.image = image
+                    self?.backgroundArtworkImageView.image = blurredImage
                     self?.updateColors(with: colors)
                 }
             }.resume()
@@ -238,24 +134,6 @@ final class ACMConcertPlayerViewController: UIViewController {
         elapsedTimeLabel.text = (elapsed_hr != "00" ? "\(elapsed_hr):" : "") + "\(elapsed_min):\(elapsed_sec)"
         totalTimeLabel.text = "-" + (remaining_hr != "00" ? "\(remaining_hr):" : "") + "\(remaining_min):\(remaining_sec)"
         progress += 1000
-    }
-    
-    func updateInfoLabel(with title: String) {
-        infoLabel.text = title
-    }
-    
-    func updatePlayPause(with isPlaying: Bool) {
-        let image = isPlaying ? #imageLiteral(resourceName: "pause") : #imageLiteral(resourceName: "play")
-        self.playPauseButton.setImage(image, for: .normal)
-    }
-    
-    func updateVolume(with slider: Int) {
-        self.volumeSlider.value = Float(slider)
-    }
-
-    func update(artworkImage: UIImage?, withBlurredArtworkImage blurredArtworkImage: UIImage?) {
-        artworkImageView.image = artworkImage
-        backgroundArtworkImageView.image = blurredArtworkImage
     }
 
     func updateColors(with colors: [UIColor]?) {
@@ -280,6 +158,38 @@ final class ACMConcertPlayerViewController: UIViewController {
     
     public func zfill(_ input: String, _ length: Int) -> String {
         // use String.init(format: String, vargs...)
-        return String(repeating: "0", count: (length - input.count)) + input
+        return String.init(format: "%0d.0s", arguments: [length, input])
     }
 }
+
+extension ACMConcertPlayerViewController: ACMConcertSocketDelegate {
+    func acmConcertSocket(_ acmConcertSocket: ACMConcertSocket, didReceivePlayStateUpdate isPlaying: Bool) {
+        let image = isPlaying ? #imageLiteral(resourceName: "pause") : #imageLiteral(resourceName: "play")
+        self.playPauseButton.setImage(image, for: .normal)
+    }
+    
+    func acmConcertSocket(_ acmConcertSocket: ACMConcertSocket, didReceiveVolumeUpdate newVolume: Int) {
+        self.volumeSlider.value = Float(newVolume)
+    }
+    
+    func acmConcertSocket(_ acmConcertSocket: ACMConcertSocket, didReceiveProgressUpdate progress: Int, didReceiveDurationUpdate duration: Int) {
+        if timer?.isValid == false {
+            timer = Timer.scheduledTimer(
+                timeInterval: 1,
+                target: self,
+                selector: #selector(updateProgress),
+                userInfo: nil,
+                repeats: true
+            )
+        }
+    }
+    
+    func acmConcertSocket(_ acmConcertSocket: ACMConcertSocket, didReceiveInfoLabel trackName: String?) {
+        infoLabel.text = trackName
+    }
+    
+    func acmConcertSocket(_ acmConcertSocket: ACMConcertSocket, didReceiveNewArtwork url: URL?) {
+        updateArtwork(with: url)
+    }
+}
+
--- office/ViewControllers/ACMConcertSocket.swift
+++ office/ViewControllers/ACMConcertSocket.swift
@@ -7,3 +7,133 @@
 //
 
 import Foundation
+import SocketIO
+
+public protocol ACMConcertSocketDelegate: class {
+    func acmConcertSocket(_ acmConcertSocket: ACMConcertSocket, didReceivePlayStateUpdate: Bool)
+    func acmConcertSocket(_ acmConcertSocket: ACMConcertSocket, didReceiveVolumeUpdate: Int)
+    func acmConcertSocket(_ acmConcertSocket: ACMConcertSocket, didReceiveProgressUpdate progress: Int, didReceiveDurationUpdate duration: Int)
+    func acmConcertSocket(_ acmConcertSocket: ACMConcertSocket, didReceiveNewArtwork: URL?)
+    func acmConcertSocket(_ acmConcertSocket: ACMConcertSocket, didReceiveInfoLabel: String?)
+}
+
+public final class ACMConcertSocket {
+    weak private var delegate: ACMConcertSocketDelegate?
+    
+    private let socketManager = SocketManager(socketURL: URL(string: "http://concert.acm.illinois.edu")!)
+    
+    public init(delegate: ACMConcertSocketDelegate? = nil) {
+        self.delegate = delegate
+        configureSocket()
+    }
+    
+    let jsonDecoder = JSONDecoder()
+    
+    // MARK: Socket Init
+    func configureSocket() {
+        socketManager.defaultSocket.on("connected",      callback: handleConnection)
+        socketManager.defaultSocket.on("heartbeat",      callback: handleConnection)
+        socketManager.defaultSocket.on("skipped",        callback: handleConnection)
+        socketManager.defaultSocket.on("volume_changed", callback: handleVolume)
+        socketManager.defaultSocket.on("paused",         callback: handlePause)
+        socketManager.defaultSocket.on("played",         callback: handlePlay)
+    }
+    
+    @objc public func setupSocket() {
+        socketManager.connect()
+    }
+    
+    @objc public func teardownSocket() {
+        socketManager.disconnect()
+    }
+    
+//    private func handle() {
+//        delegate?.acmConcertSocket(self, didReceivePlayStateChange: true)
+//    }
+    
+    // MARK: Handlers
+    func handleConnection(dataArray: [Any], ack: SocketAckEmitter) {
+        print("handle connection a")
+        
+        guard let jsonString = dataArray.first as? String,
+            let jsonData = jsonString.data(using: .utf8),
+            let status = try? jsonDecoder.decode(ACMConcertOnConnect.self, from: jsonData) else { return }
+        
+        print("handle connection b")
+        
+        let isPlaying = status.isPlaying,
+        audioStatus = status.audioStatus
+        let displayIsPlaying = isPlaying && (audioStatus == "State.Playing" || audioStatus == "State.Opening")
+        
+        let url = URL.init(string: "http://concert.acm.illinois.edu/" + status.thumbnail)
+        
+        DispatchQueue.main.async { [weak self] in
+            guard let strongSelf = self else { return }
+
+            strongSelf.delegate?.acmConcertSocket(strongSelf, didReceiveProgressUpdate: status.currentTime, didReceiveDurationUpdate: status.duration)
+            strongSelf.delegate?.acmConcertSocket(strongSelf, didReceiveNewArtwork: url)
+            strongSelf.delegate?.acmConcertSocket(strongSelf, didReceivePlayStateUpdate: displayIsPlaying)
+            strongSelf.delegate?.acmConcertSocket(strongSelf, didReceiveVolumeUpdate: status.volume)
+            strongSelf.delegate?.acmConcertSocket(strongSelf, didReceiveInfoLabel: status.currentTrack)
+        }
+    }
+    
+    func handleVolume(dataArray: [Any], ack: SocketAckEmitter) {
+        print("handle volume a")
+        
+        guard let jsonString = dataArray.first as? String,
+            let jsonData = jsonString.data(using: .utf8),
+            let status = try? jsonDecoder.decode(ACMConcertVolume.self, from: jsonData) else { return }
+        
+        print("handle volume b")
+        
+        let volume = status.volume
+        
+        DispatchQueue.main.async { [weak self] in
+            guard let strongSelf = self else { return }
+            strongSelf.delegate?.acmConcertSocket(strongSelf, didReceiveVolumeUpdate: volume)
+            
+        }
+    }
+    
+    func handlePause(dataArray: [Any], ack: SocketAckEmitter) {
+        print("handle pause a")
+        
+        guard let jsonString = dataArray.first as? String,
+            let jsonData = jsonString.data(using: .utf8),
+            let status = try? jsonDecoder.decode(ACMConcertOnPause.self, from: jsonData) else { return }
+        
+        print("handle pause b")
+        
+        let isPlaying = status.isPlaying,
+        audioStatus = status.audioStatus
+        let displayIsPlaying = isPlaying && (audioStatus == "State.Playing" || audioStatus == "State.Opening")
+        print(displayIsPlaying)
+        
+        DispatchQueue.main.async { [weak self] in
+            guard let strongSelf = self else { return }
+            strongSelf.delegate?.acmConcertSocket(strongSelf, didReceivePlayStateUpdate: displayIsPlaying)
+        }
+    }
+    
+    func handlePlay(dataArray: [Any], ack: SocketAckEmitter) {
+        print("handle play a")
+        
+        guard let jsonString = dataArray.first as? String,
+            let jsonData = jsonString.data(using: .utf8),
+            let status = try? jsonDecoder.decode(ACMConcertOnPlay.self, from: jsonData) else { return }
+        
+        print("handle play b")
+        
+        let isPlaying = status.isPlaying,
+        audioStatus = status.audioStatus
+        let displayIsPlaying = isPlaying && (audioStatus == "State.Playing" || audioStatus == "State.Opening")
+        print(displayIsPlaying)
+        
+        DispatchQueue.main.async { [weak self] in
+            guard let strongSelf = self else { return }
+            strongSelf.delegate?.acmConcertSocket(strongSelf, didReceivePlayStateUpdate: displayIsPlaying)
+        }
+    }
+    
+}
